c424f1f49eef7d1f84d11dec41d4982bcefd8b03
Pruthvi Patel <patelpruthvi.01221@gmail.com>
2024-10-11 18:07:55 -0400

    Change Port Number

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const complianceRoutes = require("./routes/complianceRoute");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
